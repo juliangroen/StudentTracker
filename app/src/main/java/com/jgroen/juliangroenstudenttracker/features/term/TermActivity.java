e0988ef2b25598e492179f74beda3b924a6ea566
Julian <julian@jgroen.com>
2020-03-29 23:35:23 -0600

    created intent for term activity fab

--- app/src/main/java/com/jgroen/juliangroenstudenttracker/features/term/TermActivity.java
+++ app/src/main/java/com/jgroen/juliangroenstudenttracker/features/term/TermActivity.java
@@ -2,12 +2,16 @@ package com.jgroen.juliangroenstudenttracker.features.term;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.jgroen.juliangroenstudenttracker.R;
+import com.jgroen.juliangroenstudenttracker.features.course.CourseActivity;
 
 public class TermActivity extends AppCompatActivity {
+
+    private static final int NEW_TERM_ACTIVITY_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +19,9 @@ public class TermActivity extends AppCompatActivity {
         setContentView(R.layout.activity_term);
 
         FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(view -> {
+            Intent intent = new Intent(TermActivity.this, CourseActivity.class);
+            startActivityForResult(intent, NEW_TERM_ACTIVITY_REQUEST_CODE);
+        });
     }
 }
