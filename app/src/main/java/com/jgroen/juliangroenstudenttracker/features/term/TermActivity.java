7c3e57cbc6ff50f7a65aa4f2223df0006d54c1e4
Julian <julian@jgroen.com>
2020-03-29 22:30:05 -0600

    updated gradel dependencies, added floating action button on term activity

--- app/src/main/java/com/jgroen/juliangroenstudenttracker/features/term/TermActivity.java
+++ app/src/main/java/com/jgroen/juliangroenstudenttracker/features/term/TermActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.jgroen.juliangroenstudenttracker.R;
 
 public class TermActivity extends AppCompatActivity {
@@ -12,5 +13,7 @@ public class TermActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_term);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
     }
 }
