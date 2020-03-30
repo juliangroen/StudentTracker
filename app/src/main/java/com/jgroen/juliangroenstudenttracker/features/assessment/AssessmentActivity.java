10f0a9d31d022f4f105a1cfbf5ec8ff94e65b486
Julian <julian@jgroen.com>
2020-03-29 21:27:41 -0600

    initial commit

---
+++ app/src/main/java/com/jgroen/juliangroenstudenttracker/features/assessment/AssessmentActivity.java
@@ -0,0 +1,16 @@
+package com.jgroen.juliangroenstudenttracker.features.assessment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.jgroen.juliangroenstudenttracker.R;
+
+public class AssessmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assessment);
+    }
+}
---
+++ app/src/main/java/com/jgroen/juliangroenstudenttracker/features/course/CourseActivity.java
@@ -0,0 +1,16 @@
+package com.jgroen.juliangroenstudenttracker.features.course;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.jgroen.juliangroenstudenttracker.R;
+
+public class CourseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course);
+    }
+}
